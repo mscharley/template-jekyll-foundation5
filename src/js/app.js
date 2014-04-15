461c0432bd8be632aecc10f69d81ae90ddc01980
Matthew Scharley <matt.scharley@gmail.com>
2014-04-15 22:38:55 +1000

    Implement RequireJS

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,17 @@
 
-$(document).load(function() {
+requirejs.config({
+  paths: {
+    modernizr: '/assets/modernizr/modernizr',
+    jquery: '/assets/jquery/dist/jquery.min',
+    foundation: '/assets/foundation/js/foundation'
+  },
+  shim: {
+    foundation: ['jquery']
+  }
+});
+
+requirejs(['jquery', 'foundation', 'modernizr'], function($) {
   // Foundation JavaScript
   // Documentation can be found at: http://foundation.zurb.com/docs
-  $(this).foundation();
+  $(document).foundation();
 });
